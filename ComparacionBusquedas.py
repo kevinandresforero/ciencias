1ad7424e5e2e97e78446f164ffec1ea3efdd8d94
kevinandresforero <kevinandresforero@gmail.com>
2024-02-20 07:58:40 -0500

    ejemplo_convertirAHash

--- ComparacionBusquedas.py
+++ ComparacionBusquedas.py
@@ -14,13 +14,13 @@ for x in range(800000):
     }
     productos.append(producto)
 
-#print(productos)
+# print(productos)
 
 idABuscar = 799999
 
 tiempoDespues = time.time()
 
-print(f"Crear Productos: {tiempoDespues - tiempoAntes}")
+print(f"\nCrear Productos: {tiempoDespues - tiempoAntes}\n")
 
 #
 
@@ -29,7 +29,7 @@ tiempoAntes = time.time()
 for producto in productos:
     encontrado = False
     if producto.get("id") == idABuscar:
-        print(producto)#info del producto
+        print(producto)  # info del producto
         encontrado = True
         break
 
@@ -46,22 +46,22 @@ print(f"Busqueda Secuencial: {tiempoDespues - tiempoAntes}")
 
 tiempoAntes = time.time()
 
-while(True):
+while (True):
     pivote = int((len(productos)-1)/2)
-    #print(f"pivote {pivote}")
+    # print(f"pivote {pivote}")
     idDatoEnPivote = productos[pivote].get("id")
-    #print(idDatoEnPivote)
-    #print(f"iDpivote {idPivote}")
-    #time.sleep(0.5)
+    # print(idDatoEnPivote)
+    # print(f"iDpivote {idPivote}")
+    # time.sleep(0.5)
     if idDatoEnPivote == idABuscar:
         print(productos[pivote])
         break
-    #if(productos[pivote+1].get("id")) > idABuscar:
+    # if(productos[pivote+1].get("id")) > idABuscar:
     if idDatoEnPivote > idABuscar:
         productos = productos[:pivote]
     else:
         productos = productos[pivote+1:]
-    #print(productos)
+    # print(productos)
     if not productos:
         print("no encontrado")
         break
@@ -71,4 +71,3 @@ tiempoDespues = time.time()
 print(f"Busqueda Binaria: {tiempoDespues - tiempoAntes}")
 
 #
-
--- funcionHash.py
+++ funcionHash.py
@@ -1,36 +1,31 @@
 import random
 import string
-import time, datetime
+import time
+import datetime
 
 productos = []
 personas = []
 
+
 def convertirAHash(ccDueno, ccComprador, fecha, idproducto):
     hashccDueno = sum([ord(x) for x in ccDueno])
     hashccComprador = sum([ord(x) for x in ccComprador])
-    hashFecha = sum([ord(x) for x in "eNeRo"])
+    hashFecha = sum([ord(x) for x in fecha])
     hashIdProducto = sum([ord(x) for x in idproducto])
-    print(hashccDueno, hashccDueno, hashFecha, hashIdProducto)
-
-print(ord("a"))
-print(ord("b"))
-print(sum([ord(x) for x in "ab"]))
+    return hashccDueno+hashccComprador+hashFecha+hashIdProducto
 
 
-print(datetime.datetime.now())
+# Usando datetime.datetime.now() para obtener fecha y hora actual
+fecha_hora_actual = str(datetime.datetime.now())
 
-
-
-convertirAHash("123", "123", "hi", "qrt")
-convertirAHash()
-
-
+print(convertirAHash("123", "123", fecha_hora_actual, "qrt"))
 
 
 class persona:
     def __init__(self, cc, nombre):
         self.cc = cc
         self.nombre = nombre
+
 
 personas.append(persona(12310, "Julanito1"))
 personas.append(persona(12320, "Julanito2"))
@@ -42,10 +37,6 @@ personas.append(persona(12370, "Julanito7"))
 personas.append(persona(12380, "Julanito8"))
 personas.append(persona(12390, "Julanito9"))
 personas.append(persona(12310, "Julanito10"))
-
-print([x.nombre for x in personas])
-
-
 
 
 tiempoAntes = time.time()
@@ -59,64 +50,8 @@ for x in range(10):
     }
     productos.append(producto)
 
-print(productos)
 
-
-"""
-idABuscar = 799999
-
-tiempoDespues = time.time()
-
-print(f"Crear Productos: {tiempoDespues - tiempoAntes}")
-
-#
-
-tiempoAntes = time.time()
-
-for producto in productos:
-    encontrado = False
-    if producto.get("id") == idABuscar:
-        print(producto)#info del producto
-        encontrado = True
-        break
-
-if not encontrado:
-    print("no encontrado")
-
-tiempoDespues = time.time()
-
-print(f"Busqueda Secuencial: {tiempoDespues - tiempoAntes}")
-
-#
-
-#
-
-tiempoAntes = time.time()
-
-while(True):
-    pivote = int((len(productos)-1)/2)
-    #print(f"pivote {pivote}")
-    idDatoEnPivote = productos[pivote].get("id")
-    #print(idDatoEnPivote)
-    #print(f"iDpivote {idPivote}")
-    #time.sleep(0.5)
-    if idDatoEnPivote == idABuscar:
-        print(productos[pivote])
-        break
-    #if(productos[pivote+1].get("id")) > idABuscar:
-    if idDatoEnPivote > idABuscar:
-        productos = productos[:pivote]
-    else:
-        productos = productos[pivote+1:]
-    #print(productos)
-    if not productos:
-        print("no encontrado")
-        break
-
-tiempoDespues = time.time()
-
-print(f"Busqueda Binaria: {tiempoDespues - tiempoAntes}")
-
-#
-
-"""
+class transaccion:
+    def __init__(self, ccDueño, ccComprador):
+        self.id = ccDueño + ccComprador + str(datetime.datetime)
+        self.detalle = ccDueño + ccComprador + str(datetime.datetime)
