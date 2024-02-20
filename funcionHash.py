09c60935b75429e2edda6833920a313811f270f8
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2024-02-20 11:55:30 -0500

    Add files via upload

--- funcionHash.py
+++ funcionHash.py
@@ -5,23 +5,37 @@ import datetime
 
 productos = []
 personas = []
-transacciones = []
+transacciones = [None] * 20
 
 
-def convertirAHash(ccDueno, ccComprador, idproducto, fecha):
+def crecerArregloTransacciones(idHash, arregloTransacciones):
+    arregloConTamanioCorregido = arregloTransacciones
+    if idHash > (len(arregloTransacciones) - 1):
+        loQueLeFalta = idHash - (len(arregloTransacciones) - 1)
+        #print(loQueLeFalta)
+        arregloTemp = [None] * loQueLeFalta
+        #print(arregloTemp)
+        arregloConTamanioCorregido += arregloTemp
+        #print(len(arregloConTamanioCorregido))
+    return arregloConTamanioCorregido
+
+
+
+def convertirAHash(ccDueno, ccComprador, idproducto):
     primo1 = 31
     primo2 = 37
     primo3 = 41
     primo4 = 43
 
     # Convertimos cada parte del input en un hash numérico simple sumando los valores ASCII de cada carácter.
-    hashccDueno = sum([ord(x) for x in ccDueno]) * primo1
-    hashccComprador = sum([ord(x) for x in ccComprador]) * primo2
-    hashFecha = sum([ord(x) for x in fecha]) * primo3
-    hashIdProducto = sum([ord(x) for x in idproducto]) * primo4
+    hashccDueno = sum([ord(x) for x in str(ccDueno)]) * primo1
+    hashccComprador = sum([ord(x) for x in str(ccComprador)]) * primo2
+    #hashFecha = sum([ord(x) for x in fecha]) * primo3
+    hashIdProducto = sum([ord(x) for x in str(idproducto)]) * primo4
 
     # Combinamos los hashes con una operación simple. Podrías elegir otras operaciones para combinar estos valores.
-    hashFinal = hashccDueno + hashccComprador + hashFecha + hashIdProducto
+    #hashFinal = hashccDueno + hashccComprador + hashFecha + hashIdProducto
+    hashFinal = hashccDueno + hashccComprador + hashIdProducto
 
     return hashFinal
 
@@ -58,42 +72,91 @@ for x in range(10):
 
 
 class transaccion:
-    def __init__(self, ccDuenoE, ccComprador, idProductoE):
-        self.fecha = fecha_hora_actual = str(datetime.datetime.now())
+    def __init__(self, ccDuenoE, ccComprador, idProductoE, anio, mes, dia):
+        #self.fecha = fecha_hora_actual = str(datetime.datetime.now())
+        self.fecha = "".join([str(anio),str(mes),str(dia)])
+        self.id = 1
+        """
         self.id = convertirAHash(
-            str(ccDuenoE), str(ccComprador), str(idProductoE), self.fecha)
+            str(ccDuenoE), str(ccComprador), str(idProductoE))
+        """
         self.ccDueño = ccDuenoE
         self.ccComprador = ccComprador
         self.idProducto = idProductoE
+        self.Siguiente = None
+
+class listaEnlazadaTransacciones:
+    def __init__(self):
+        self.nodoCabeza = None
+        self.nodoCola = None
+
+    def agregarTransaccion(self, objetoTransaccion):
+        if self.nodoCola == None:
+            self.nodoCola = objetoTransaccion
+        else:
+            self.nodoCola.Siguiente = objetoTransaccion
+            self.nodoCola = objetoTransaccion
+
+        if self.nodoCabeza == None:
+            self.nodoCabeza = objetoTransaccion
+            self.nodoCabeza.Siguiente = self.nodoCola
+
+linkedListTransacciones = listaEnlazadaTransacciones()
 
 
-if productos:
-    producto_aleatorio = random.choice(productos)
-    # Obtiene el cc del dueño del producto seleccionado
-    cc_dueño_producto_aleatorio = producto_aleatorio['dueno']
-    # Selecciona un comprador aleatorio diferente al dueño actual
-    compradores_potenciales = [
-        persona for persona in personas if persona.cc != cc_dueño_producto_aleatorio]
-    if not compradores_potenciales:  # Verifica si hay compradores potenciales
-        print("No hay compradores potenciales disponibles.")
-        exit()
-    comprador_aleatorio = random.choice(compradores_potenciales)
-    # Crea la transacción con el dueño actual, un comprador aleatorio, y el ID del producto aleatorio
-    nueva_transaccion = transaccion(
-        cc_dueño_producto_aleatorio, comprador_aleatorio.cc, producto_aleatorio['id'])
-    transacciones.append(nueva_transaccion)  # Guarda la nueva transacción
+for repeticiones in range(5):
+    if productos:
+        producto_aleatorio = random.choice(productos)
+        # Obtiene el cc del dueño del producto seleccionado
+        cc_dueño_producto_aleatorio = producto_aleatorio['dueno']
+        # Selecciona un comprador aleatorio diferente al dueño actual
+        compradores_potenciales = [
+            persona for persona in personas if persona.cc != cc_dueño_producto_aleatorio]
+        if not compradores_potenciales:  # Verifica si hay compradores potenciales
+            print("No hay compradores potenciales disponibles.")
+            exit()
+        comprador_aleatorio = random.choice(compradores_potenciales)
+        # Crea la transacción con el dueño actual, un comprador aleatorio, y el ID del producto aleatorio
+        nueva_transaccion = transaccion(
+            cc_dueño_producto_aleatorio, comprador_aleatorio.cc, producto_aleatorio['id'], 2024, 1, 1)
+        #idHashTemp = convertirAHash(nueva_transaccion.ccDueño, nueva_transaccion.ccComprador, nueva_transaccion.idProducto)
+        idHashTemp = 15000
+        #print(idHashTemp)
+        print(len(transacciones))
+        transacciones = crecerArregloTransacciones(idHashTemp, transacciones)
+        print(len(transacciones))
+        #transacciones[idHashTemp] = nueva_transaccion
+        if transacciones[idHashTemp] == None:
+            transacciones[idHashTemp] = listaEnlazadaTransacciones()
+            transacciones[idHashTemp].agregarTransaccion(nueva_transaccion)
+        else:
+            transacciones[idHashTemp].agregarTransaccion(nueva_transaccion)
+        
+        print(transacciones[idHashTemp-5:idHashTemp +5])
+        #linkedListTransacciones.agregarTransaccion(nueva_transaccion)
+        #transacciones.append(nueva_transaccion)  # Guarda la nueva transacción // Debeira convertir a hash
 
-    # Actualiza el dueño del producto en la lista de productos
-    for producto in productos:
-        if producto['id'] == producto_aleatorio['id']:
-            # Actualiza el dueño del producto al comprador
-            producto['dueno'] = comprador_aleatorio.cc
-            break  # Sale del bucle una vez que el producto ha sido actualizado
+        # Actualiza el dueño del producto en la lista de productos
+        """
+        for producto in productos:
+            if producto['id'] == producto_aleatorio['id']:
+                # Actualiza el dueño del producto al comprador
+                producto['dueno'] = comprador_aleatorio.cc
+                break  # Sale del bucle una vez que el producto ha sido actualizado
+        """
 
-else:
-    print("No hay productos disponibles para crear una transacción.")
+    else:
+        print("No hay productos disponibles para crear una transacción.")
 
+nodoTemp = transacciones[-1].nodoCabeza
 
+while True:
+    print(nodoTemp)
+    nodoTemp = nodoTemp.Siguiente
+    if nodoTemp == linkedListTransacciones.nodoCola:
+        break
+
+"""
 def imprimir_transacciones(transacciones):
     if not transacciones:
         print("No se han realizado transacciones.")
@@ -111,3 +174,5 @@ def imprimir_transacciones(transacciones):
 
 # Llama a la función para imprimir las transacciones
 imprimir_transacciones(transacciones)
+
+"""
